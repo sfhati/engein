7a85220b51c3ce5313e8945cf17241266cbf73d8
bassam alessawi <sfhati.com@gmail.com>
2025-09-09 22:42:05 +0300

    Update syntax.php

--- syntax.php
+++ syntax.php
@@ -34,244 +34,272 @@
  * @param boolean $write_source defult true , write template code in export php file as commant
  * @return string php code  
  */
-class __SYNTAX {
-
-    private $ALL_SYNTAX = '';
-    private $cache_path = '';
-    public $filename = '';
-
-    public function __construct($cache_path = '') {
-        if ($cache_path)
-            $this->cache_path = $cache_path;
-        else
-            $this->cache_path = __DIR__ . DIRECTORY_SEPARATOR . 'cache' . DIRECTORY_SEPARATOR;
+class ModernSyntaxEngine {
+    
+    private string $allSyntax = '';
+    private string $cachePath = '';
+    private string $filename = '';
+    
+    public function __construct(string $cachePath = '') {
+        if ($cachePath) {
+            $this->cachePath = rtrim($cachePath, '/\\') . DIRECTORY_SEPARATOR;
+        } else {
+            $this->cachePath = __DIR__ . DIRECTORY_SEPARATOR . 'cache' . DIRECTORY_SEPARATOR;
+        }
+        
+        // Create cache directory if it doesn't exist
+        if (!is_dir($this->cachePath)) {
+            mkdir($this->cachePath, 0755, true);
+        }
     }
-
+    
     /**
-     * open template file 
+     * Open template file and process it
      *
-     * @param string $templatefile full path for template file
-     * @return string php code  
+     * @param string $templateFile Full path for template file
+     * @return string Path to compiled PHP file or error message
      */
-    public function openfile($templatefile) {
-        //check if exist file template
-        if (!file_exists($templatefile))
-            return 'File not found!';
-
-        $export_php_file = $this->cache_path . md5($templatefile) . '_' . md5_file($templatefile) . '.php';
-        //check if already translate 
-        if (file_exists($export_php_file))
-            return $export_php_file;
-
-        $this->filename = $templatefile;
-
-        //open file & get content to ALL_SYNTAX        
-        $this->ALL_SYNTAX = implode(file($templatefile), '');
-        // if there is php code will show as content in output
-        $this->ALL_SYNTAX = str_replace(array('<?','?>'), array('&lt;?','?&gt;'), $this->ALL_SYNTAX);
-        //start translate template code
-        $t = $this->Syntax($this->ALL_SYNTAX);
-        //delete old php files from cache folder
-        $this->clearcache(md5($templatefile));
-        //write php file
-        $this->filewrite($export_php_file, $t);
-        return $export_php_file;
+    public function openFile(string $templateFile): string {
+        // Check if template file exists
+        if (!file_exists($templateFile)) {
+            return 'Template file not found: ' . $templateFile;
+        }
+        
+        $exportPhpFile = $this->cachePath . md5($templateFile) . '_' . md5_file($templateFile) . '.php';
+        
+        // Check if already compiled and up to date
+        if (file_exists($exportPhpFile)) {
+            return $exportPhpFile;
+        }
+        
+        $this->filename = $templateFile;
+        
+        // Read template file content
+        $this->allSyntax = file_get_contents($templateFile);
+        if ($this->allSyntax === false) {
+            return 'Error reading template file: ' . $templateFile;
+        }
+        
+        // Escape existing PHP tags to prevent code injection
+        $this->allSyntax = str_replace(['<?', '?>'], ['&lt;?', '?&gt;'], $this->allSyntax);
+        
+        // Process template syntax
+        $compiledCode = $this->processSyntax($this->allSyntax);
+        
+        // Clean old cache files
+        $this->clearCache(md5($templateFile));
+        
+        // Write compiled PHP file
+        if ($this->writeFile($exportPhpFile, $compiledCode)) {
+            return $exportPhpFile;
+        }
+        
+        return 'Error writing compiled template file';
     }
-
+    
     /**
-     * Clear php file from cache folder 
-     * 
-     *
-     * @param string $file full path and file name     
-     * @return none!  
+     * Clear old cache files for this template
      */
-    private function clearcache($templatefile) {
-        $dir = $this->cache_path;
-        if ($dh = opendir($dir)) {
-
-            while (($file = readdir($dh)) !== false) {
-                $pathtemplate = explode('_', $file);
-                if ($pathtemplate[0] == $templatefile) {
-                    @unlink($dir . $file);
+    private function clearCache(string $templateHash): void {
+        if (!is_dir($this->cachePath)) {
+            return;
+        }
+        
+        $files = scandir($this->cachePath);
+        if ($files === false) {
+            return;
+        }
+        
+        foreach ($files as $file) {
+            if ($file === '.' || $file === '..') {
+                continue;
+            }
+            
+            $pathParts = explode('_', $file);
+            if (isset($pathParts[0]) && $pathParts[0] === $templateHash) {
+                $fullPath = $this->cachePath . $file;
+                if (is_file($fullPath)) {
+                    unlink($fullPath);
                 }
             }
-            closedir($dh);
         }
     }
-
+    
     /**
-     * translate syntax form template code      
-     *
-     * @param string $t syntax code
-     * @return string php code  
+     * Process template syntax recursively
      */
-    public function Syntax($t) {
-        $syn = $this->GetSyantax($t);
-        if (is_array($syn) && $syn[0]) {
-            $orginalSyn = $syn[1] . $syn[3] . $syn[2];
-            $vars = $this->parse_variable('[XXX:"' . $syn[3] . '"end XXX]', $syn[0]);
-            // $vars : $syn[0]; here must go to function
-            $t = str_replace($orginalSyn, $vars, $t);
-
-            return $this->Syntax($t);
-        } else {
-            return $t;
+    public function processSyntax(string $content): string {
+        $syntax = $this->getSyntaxCommand($content);
+        
+        if (!empty($syntax) && isset($syntax[0])) {
+            $originalSyntax = $syntax[1] . $syntax[3] . $syntax[2];
+            $compiledCode = $this->parseVariables('[XXX:"' . $syntax[3] . '"end XXX]', $syntax[0]);
+            
+            $content = str_replace($originalSyntax, $compiledCode, $content);
+            
+            return $this->processSyntax($content);
         }
-        return $t;
+        
+        return $content;
     }
-
+    
     /**
-     * pares first syntac form template code 
-     *
-     * @param string $t template code
-     * @return array first syntax code , command
+     * Get the first syntax command from template content
      */
-    private function GetSyantax($t) {
-        //search for pattern like [if:"
-        preg_match("/\[(\w)+:\"/", $t, $out);
-
-        $word = str_replace('[', '', $out[0]);
-        $word = str_replace(':"', '', $word);
-        // nothing to return if not found syntax 
-        if (!$word)
-            return '';
-
-        //not our syntax so convert it to html code 
-        if (!function_exists(trim($word) . '_SYNTAX')) {
-            $t = str_replace("[{$word}:", "&#91;{$word}:&#93;", $t);
-            $t = str_replace("{$word}]", "{$word}&#93;", $t);
-            // search agean in template code 
-            return $this->GetSyantax($t);
+    private function getSyntaxCommand(string $content): array {
+        // Search for pattern like [command:"
+        if (!preg_match('/\[(\w+):"/', $content, $matches)) {
+            return [];
         }
-
-        //start syntax with command
-        $start = "[{$word}:";
-        $end = "end {$word}]";
-        //cut syntax level 1
-        $xsyn = $this->extractBetweenDelimetersx($t, $start, $end);
-        //return array
-        $return[0] = $word;
-        $return[1] = $start;
-        $return[2] = $end;
-        $return[3] = $xsyn;
-        return $return;
-    }
-
-    /**
-     * Cut string between tow words !
-     *
-     * @param string $inputstr template code
-     * @param string $delimeterLeft start word to cut
-     * @param string $delimeterRight end word to cut
-     * @return integer $i defult 1 , there is last offset postion for end word
-     */
-    private function extractBetweenDelimetersx($inputstr, $delimeterLeft, $delimeterRight, $i = 1) {
-        //get start word postion
-        $posLeft = strpos($inputstr, $delimeterLeft);
-        //get end word postion
-        $posRight = $this->strnpos($inputstr, $delimeterRight, $i);
-        //if not found return error template code
-        if ($posLeft === false || $posRight === false)
-            if ($posLeft === false)
-                die("Error:<br> near $delimeterLeft  _ !$posLeft || !$posRight _ <hr> $inputstr ");
-            else
-                die("Error:<br> near $delimeterRight _ !$posLeft || !$posRight _<hr> $inputstr ");
-        //cut string from start postion to end one    
-        $cut_text = substr($inputstr, $posLeft + strlen($delimeterLeft), $posRight - ($posLeft + strlen($delimeterLeft)));
-        $i++;
-        if ($i == 1000)
-            exit();
-        //check if there is same word on string to complete syntact 
-        if ($this->strnpos($cut_text, $delimeterLeft, $i - 1) > 0)
-            return $this->extractBetweenDelimetersx($inputstr, $delimeterLeft, $delimeterRight, $i);
-        else
-            return $cut_text;
-    }
-
-    /**
-     * help function extractBetweenDelimetersx to find a strpos
-     *
-     * @param string $haystack template code
-     * @param string $needle  word to search
-     * @param integer $nth 
-     * @return integer $offset defult 0 
-     */
-    private function strnpos($haystack, $needle, $nth, $offset = 0) {
-        if (!$haystack)
-            return "";
-        else {
-            for ($retOffs = $offset - 1; ($nth > 0) && ($retOffs !== FALSE); $nth--)
-                $retOffs = strpos($haystack, $needle, $retOffs + 1);
-            return $retOffs;
+        
+        $command = $matches[1];
+        
+        // Check if function exists for this command
+        $functionName = $command . '_SYNTAX';
+        if (!function_exists($functionName)) {
+            // Convert unknown syntax to HTML entities to prevent issues
+            $content = str_replace("[{$command}:", "&#91;{$command}:", $content);
+            $content = str_replace("end {$command}]", "end {$command}&#93;", $content);
+            return $this->getSyntaxCommand($content);
         }
+        
+        $startTag = "[{$command}:";
+        $endTag = "end {$command}]";
+        
+        // Extract syntax content
+        $syntaxContent = $this->extractBetweenDelimiters($content, $startTag, $endTag);
+        
+        return [
+            0 => $command,
+            1 => $startTag,
+            2 => $endTag,
+            3 => $syntaxContent
+        ];
     }
-
+    
     /**
-     * get syntax code and parse the variables for level 1 of syntax by replacing , with |,| 
-     * this function pattrn writen by Casimir et Hippolyte http://stackoverflow.com/users/2255089/casimir-et-hippolyte
-     *
-     * @param string $t syntax code
-     * @param string $command level 1 command syntax like if,for,each,var or other
-     * @return string php code  
+     * Extract content between delimiters with proper nesting support
      */
-    private function parse_variable($t, $command) {
+    private function extractBetweenDelimiters(string $input, string $leftDelim, string $rightDelim, int $occurrence = 1): string {
+        $leftPos = strpos($input, $leftDelim);
+        if ($leftPos === false) {
+            throw new RuntimeException("Left delimiter not found: {$leftDelim}");
+        }
+        
+        $rightPos = $this->findNthOccurrence($input, $rightDelim, $occurrence);
+        if ($rightPos === false) {
+            throw new RuntimeException("Right delimiter not found: {$rightDelim}");
+        }
+        
+        $startPos = $leftPos + strlen($leftDelim);
+        $content = substr($input, $startPos, $rightPos - $startPos);
+        
+        // Check for nested delimiters
+        if ($occurrence < 100 && strpos($content, $leftDelim) !== false) {
+            return $this->extractBetweenDelimiters($input, $leftDelim, $rightDelim, $occurrence + 1);
+        }
+        
+        return $content;
+    }
+    
+    /**
+     * Find nth occurrence of needle in haystack
+     */
+    private function findNthOccurrence(string $haystack, string $needle, int $nth, int $offset = 0): int|false {
+        for ($i = 0; $i < $nth; $i++) {
+            $pos = strpos($haystack, $needle, $offset);
+            if ($pos === false) {
+                return false;
+            }
+            $offset = $pos + 1;
+        }
+        return $pos ?? false;
+    }
+    
+    /**
+     * Parse variables and call appropriate syntax function
+     */
+    private function parseVariables(string $syntaxString, string $command): string {
         $pattern = '~
-# this part defines subpatterns to be used later in the main pattern
-(?(DEFINE)
-    (?<nestedBrackets> \[ [^][]* (?:\g<nestedBrackets>[^][]*)*+ ] )
-)
-
-# the main pattern
-(?:            # two possible entry points
-    \G(?!\A)   # 1. contiguous to a previous match
-  |            #   OR
-    [^[]* \[   # 2. all characters until an opening bracket
-)
-
-# all possible characters until "," or the closing bracket:
-[^]["]* # all that is not ] [ or "
-(?:
-    \g<nestedBrackets> [^]["]* # possible nested brackets
-  |                            #   OR
-    "(?!,") [^]["]*            # a quote not followed by ,"
-)*+  # repeat as needed
-\K   # remove all on the left from match result
-(?:
-    ","           # match the target
-  |
-    ] (*SKIP)(*F) # closing bracket: break the contiguity
-)
-~x';
-
-        // replace , with |,| for main syntax 
-        $t = preg_replace($pattern, '|,|', $t);
-        $t = str_replace('[XXX:"', '', $t);
-        $t = str_replace('"end XXX]', '', $t);
-
-        // call command function with array vars 
-        $com = $command . '_SYNTAX';         
-        return $com(explode('|,|', trim($t, '"')));
-    }
-
-    /**
-     * write output file php 
-     * 
-     *
-     * @param string $file full path and file name
-     * @param string $content php code content
-     * @return none!  
-     */
-    private function filewrite($file, $content) {
-        @unlink($file);
-        $fp = fopen($file, 'w');
-        if (flock($fp, LOCK_EX | LOCK_NB)) {
-            fwrite($fp, $content);
-            fflush($fp);
-            flock($fp, LOCK_UN);
+        (?(DEFINE)
+            (?<nestedBrackets> \[ [^][]* (?:\g<nestedBrackets>[^][]*)*+ ] )
+        )
+        
+        (?:
+            \G(?!\A)
+          |
+            [^[]* \[
+        )
+        
+        [^]["]* 
+        (?:
+            \g<nestedBrackets> [^][]*
+          |
+            "(?!,") [^]["]*
+        )*+
+        \K
+        (?:
+            ","
+          |
+            ] (*SKIP)(*F)
+        )
+        ~x';
+        
+        // Replace commas with delimiter
+        $processed = preg_replace($pattern, '|,|', $syntaxString);
+        $processed = str_replace(['[XXX:"', '"end XXX]'], '', $processed);
+        
+        // Call command function with parsed variables
+        $functionName = $command . '_SYNTAX';
+        if (function_exists($functionName)) {
+            $variables = explode('|,|', trim($processed, '"'));
+            return $functionName($variables);
         }
-        fclose($fp);
+        
+        return "<!-- Unknown syntax command: {$command} -->";
     }
-
+    
+    /**
+     * Write content to file with proper locking
+     */
+    private function writeFile(string $filename, string $content): bool {
+        // Remove existing file
+        if (file_exists($filename)) {
+            unlink($filename);
+        }
+        
+        $handle = fopen($filename, 'w');
+        if (!$handle) {
+            return false;
+        }
+        
+        if (flock($handle, LOCK_EX)) {
+            fwrite($handle, $content);
+            fflush($handle);
+            flock($handle, LOCK_UN);
+            fclose($handle);
+            return true;
+        }
+        
+        fclose($handle);
+        return false;
+    }
+    
+    /**
+     * Get template filename (for compatibility)
+     */
+    public function getFilename(): string {
+        return $this->filename;
+    }
 }
 
-
+// Maintain backward compatibility
+class __SYNTAX extends ModernSyntaxEngine {
+    public function openfile(string $templateFile): string {
+        return $this->openFile($templateFile);
+    }
+    
+    public function Syntax(string $content): string {
+        return $this->processSyntax($content);
+    }
+}
